8f328e6ec3c39e62d07337a4b5d57c832960aac5
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-21 17:26:45 +0100

    .

--- 0x07-rotate_2d_matrix/main_0.py
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -8,10 +8,7 @@ if __name__ == "__main__":
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
-    # matrix = [[10, 11, 12],
-    #           [1, 4, 7, 10],
-    #           [2, 5, 8, 11],
-    #           [3, 6, 9, 12]]
+
 
     rotate_2d_matrix(matrix)
     for row in matrix:
