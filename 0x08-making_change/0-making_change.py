3b336e94d81ba4bb75eee80352f9258025ee9bc6
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-30 15:12:55 +0100

    Commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,9 +9,16 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for x in range(1, total + 1):
-        for c in coins:
-            if x >= c:
-                dp[x] = min(dp[x], dp[x - c] + 1)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    print(dp[total] if dp[total] != float('inf') else -1)
+    # for x in range(1, total + 1):
+    #     for c in coins:
+    #         if x >= c:
+    #             dp[x] = min(dp[x], dp[x - c] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+print(makeChange([1, 2, 25], 37))
+print(makeChange([1256, 54, 48, 16, 102], 1453))
